5b3553f262c0ab252fe2281aba04cdb93d5b3765
Woojung0618 <eyes_cat@naver.com>
2021-05-19 02:38:59 +0900

    LeftComp: router설정 및 dailyComp 생성

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ export { default as BadgeComp } from "./BadgeComp";
 export { default as ConcentGraphComp } from "./ConcentGraphComp";
 export { default as MonthlyComp } from "./MonthlyComp";
 export { default as WeeklyComp } from "./WeeklyComp";
+export { default as DailyComp } from "./DailyComp";
 export { default as LeftStudyComp } from "./LeftStudyComp";
 export { default as RightStudyComp } from "./RightStudyComp";
