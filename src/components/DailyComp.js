9f1755b081be0a7b489812548a7a9405ffba1666
Woojung0618 <eyes_cat@naver.com>
2021-05-20 15:16:21 +0900

    Daily: get,post,delete,put(only check)

--- src/components/DailyComp.js
+++ src/components/DailyComp.js
@@ -8,6 +8,8 @@ import React, {
 } from 'react';
 import { AuthContext } from "../App";
 import '../css/LeftStudy.css';
+import { getApi, postApi, putApi, deleteApi } from "../api";
+import { GrCheckbox, GrCheckboxSelected } from "react-icons/gr";
 
 const TenMinPlanner = () => {
     return (
@@ -16,22 +18,29 @@ const TenMinPlanner = () => {
 };
 
 const DailyContext = createContext(null);
-const initialData = [
-    {
-        id: 1,
-        d_date: "2021-05-19",
-        d_content: "졸작 회의",
-        d_check: false,
-        d_tag: "3-5"
-    },
-    {
-        id: 2,
-        d_date: "2021-05-109",
-        d_content: "졸작 회의",
-        d_check: true,
-        d_tag: ""
-    },
-];
+
+// const initialData = {
+//     dtodos: [],
+// }
+// dummy data
+const initialData = {
+    dtodos: [
+        {
+            id: 1,
+            d_date: '2021-05-20',
+            d_content: '졸작회의',
+            d_check: true,
+            d_tag: '3~5',
+        },
+        {
+            id: 2,
+            d_date: '2021-05-20',
+            d_content: '졸작회의',
+            d_check: false,
+            d_tag: '',
+        },
+    ]
+};
 
 function reducer(state, action) {
     switch (action.type) {
@@ -45,22 +54,23 @@ function reducer(state, action) {
             }));
             
         case 'CREATE_TODO':
-            return state.map((d, i) => {
-                return {
-                    ...d,
-                    id: action.id,
-                    d_date: action.d_date,
-                    d_tag: action.d_tag,
-                    d_content: action.d_content,
-                    d_check: action.d_check,
-                }
-            });
+            return {
+                dtodos: state.dtodos.concat(action.dtodo)
+            };
         case 'CHECK_TODO':
             return {
-
+                ...state,
+                dtodos: state.dtodos.map(dtodo => 
+                    dtodo.id === action.id ? {
+                         ...dtodo, 
+                         d_check: !dtodo.d_check 
+                    } : dtodo
+                )
             };
         case 'DELETE_TODO':
             return {
+                ...state,
+                dtodos: state.dtodos.filter(dtodo => dtodo.id !== action.id)
             };
         
         default:
@@ -70,7 +80,21 @@ function reducer(state, action) {
 
 const PrintToday = () => {
     const today = new Date();
-    const result = "".concat(today.getFullYear(), '년 ', today.getMonth() + 1, '월 ', today.getDate(), '일 ');
+    var strDay = '';
+    switch(today.getDay()){
+        case 0:    strDay='(일)';
+        case 1:    strDay='(월)';
+        case 2:    strDay='(화)';
+        case 3:    strDay='(수)';
+        case 4:    strDay='(목)';
+        case 5:    strDay='(금)';
+        case 6:    strDay='(토)';
+    }
+    const result = "".concat(
+        today.getFullYear(), '년 ', 
+        today.getMonth() + 1, '월 ', 
+        today.getDate(), '일 ', 
+        strDay);
     return (
         <div>
             {result}
@@ -78,40 +102,69 @@ const PrintToday = () => {
     );
 };
 
+const DateFormat = () => {
+    const today = new Date();
+    const result = "".concat(
+        today.getFullYear(), '-', 
+        today.getMonth() + 1, '-', 
+        today.getDate());
+    return result;
+};
+
 const DtodosInput = () => {
-    const nextId = useRef(3);
+    // const nextId = useRef(3);
     const dailyContext = useContext(DailyContext);
-    const [dailyData, dispatch] = useReducer(reducer, initialData);
+    const authContext = useContext(AuthContext);
+    
     const [inputs, setInputs] = useState({
-        tag: "",
-        content: "",
+        tag: '',
+        content: '',
     });
 
-    const addBtnHandler = () => {
-        
-        dailyContext.dispatch({
-            type: "CREATE_TODO",
-            id: nextId.current,
-            d_date: '2021-05-19',
-            d_content: inputs.content,
-            d_tag: inputs.tag,
-            d_check: false,
-        });
+    const addBtnHandler = async () => {
+        const { status, data } = await postApi(
+            {
+                uid: authContext.state.uid,
+                d_date: DateFormat(),
+                d_content: inputs.content,
+                d_tag: inputs.tag,
+                d_check: 'F',
+            },
+            "/tdl/weekly/"
+        );
+        // dummy data
+        // const { status, data } = {
+        //     status: 200,
+        //     data: { d_todo_id: "1" },
+        // };
+        if (status === 200) {
+            dailyContext.dispatch({
+                type: "CREATE_TODO",
+                dtodo: {
+                    id: data.d_todo_id,
+                    d_date: DateFormat(),
+                    d_content: inputs.content,
+                    d_tag: inputs.tag,
+                    d_check: false,
+                }
+            });
+        } else {
+            alert("인터넷 연결이 불안정합니다.");
+        };
 
         setInputs({
             tag: '',
             content: '',
         });
 
-        nextId.current += 1;
-    }
+        // nextId.current += 1;
+    };
 
     return (
         <div className='daily-lower-input'>
-        <div className='daily-todo-input'>
             <input
                 name='tag'
-                className='daily-todo-input-tag'
+                style={{width: '30%'}}
                 onChange={(e) =>
                     setInputs({ ...inputs, tag: e.target.value })
                 }
@@ -119,52 +172,100 @@ const DtodosInput = () => {
             />
             <input
                 name='content'
-                className='daily-todo-input-context'
+                style={{width: '60%'}}
                 onChange={(e) =>
                     setInputs({ ...inputs, content: e.target.value })
                 }
                 value={inputs.content}
             />
-            <button onClick={addBtnHandler} className="add-button">+</button>
+            <button 
+                onClick={addBtnHandler} 
+                className="add-button"
+                >+</button>
         </div>
-        </div> 
     );
 };
 
 const DTodosItem = ({ id, d_date, d_content, d_tag, d_check }) => {
-    // const dailyContext = useContext(DailyContext);
-    const clickCheck = () => {
-        console.log('check');
-    }
-    const clickDelete = () => {
-        console.log('delete');
-    }
+    const dailyContext = useContext(DailyContext);
+    const authContext = useContext(AuthContext);
+    
+    const clickCheck = async () => {
+        const { status, data } = await putApi(
+            {
+                uid: authContext.state.uid,
+                d_todo_id: id,
+                d_content: d_content,
+                d_tag: d_tag,
+                d_check: !d_check === false ? "F" : "T",
+                d_date: d_date,
+            },
+            "/tdl/daily/"
+        );
+        if (status === 200) {
+            await dailyContext.dispatch({
+                type: 'CHECK_TODO',
+                id,
+            });
+        } else {
+            alert("인터넷 연결 불안정");
+        }
+    };
+
+
+    const clickDelete = async () => {
+        const { status, data } = await deleteApi(
+            { 
+                uid: authContext.state.uid, 
+                d_todo_id: id 
+            },
+            "/tdl/daily/"
+        );
+
+        if (status === 200) {
+            await dailyContext.dispatch({
+                type: 'DELETE_TODO',
+                id,
+            });
+        } else {
+            await alert("네트워크 불안정");
+        }
+    };
+
     return (
-        <div className='daily-todo-input'>
-            <span className='daily-todo-tag'>{d_tag}</span>
-            <span className='daily-todo-content'>{d_content}</span>
-            <span 
-                className='daily-todo-check'
-                onClick={clickCheck}
-                >
-                {(d_check) ? <span></span>:<span>✔</span>}
+        <div key={id} className='daily-todo-item'>
+            <span style={{width: '30%'}}>
+                {d_tag}
             </span>
+            <span style={{width: '50%'}}>
+                {d_content}
+            </span>
+            {d_check ? (
+                <GrCheckboxSelected
+                    onClick={clickCheck}
+                    className="daily-checkbox"
+                />
+            ) : (
+                <GrCheckbox onClick={clickCheck} className="daily-checkbox" />
+            )}
             <span 
-                className='daily-todo-delete'
+                style={{width: '5%'}, {paddingLeft:'1vmin'}, {cursor: 'pointer'}}
                 onClick={clickDelete}
                 >❌
             </span>
+            
         </div>
     );
 };
 
 const DTodosList = ({ id, d_date, d_content, d_tag, d_check }) => {
     // const dailyContext = useContext(DailyContext);
-
+    
     return (
         <div>
             <DTodosItem 
                 key={id}
+                id={id}
                 d_date={d_date}
                 d_content={d_content}
                 d_tag={d_tag}
@@ -176,24 +277,54 @@ const DTodosList = ({ id, d_date, d_content, d_tag, d_check }) => {
 
 
 const DailyComp = () => {
-    const [dailyData, dispatch] = useReducer(reducer, initialData);
-    console.log(dailyData);
+    const [state, dispatch] = useReducer(reducer, initialData);
+    // const nextId = useRef(3);
+    const { dtodos } = state;
 
+    const authContext = useContext(AuthContext);
+
+    useEffect(() => {
+        const getDailyData = async () => {
+            const { status, data } = await getApi(
+                {
+                    uid: authContext.state.uid,
+                    dates: DateFormat(),
+                },
+                "/tdl/daily/"
+            );
+            // await console.log("아아아");
+            if (status === 200) {
+                await dispatch({
+                    type: "GET_TODO",
+                    d_todo_list: data.d_todo_list,
+                });
+            } else {
+                await console.log(status, data);
+                alert("인터넷 연결이 불안정합니다.");
+            }
+        };
+        getDailyData();
+    }, []);
 
     return (
         <div className="daily-comp">
             <PrintToday />
             <div className="daily-study-comp">
                 <div className="daily-planner">
-                    <div className='daily-todo-input'>
-                        <span className='daily-todo-input-tag'>태그</span>
-                        <span className='daily-todo-input-context'>할 일</span>
+                    <div className='daily-todo-upper'>
+                        <span style={{width: '30%'}}>
+                            시간대
+                        </span>
+                        <span style={{width: '70%'}}>
+                            할 일
+                        </span>
                     </div>
-                    <DailyContext.Provider value={{ dailyData, dispatch }}>
+                    <DailyContext.Provider value={{ dtodos, dispatch }}>
                         <div>
-                            {dailyData.map((d, i) => (
+                            {dtodos.map( d => (
                                 <DTodosList
                                     key={d.id}
+                                    id={d.id}
                                     d_date={d.d_date}
                                     d_content={d.d_content}
                                     d_tag={d.d_tag}
