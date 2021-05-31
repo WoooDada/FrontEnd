bb80a260d12aca0d524656cffa291f99d91afe0b
Woojung0618 <eyes_cat@naver.com>
2021-06-01 01:25:16 +0900

    Badge:Modal

--- src/components/BadgeComp.js
+++ src/components/BadgeComp.js
@@ -5,6 +5,7 @@ import { FaMedal } from "react-icons/fa";
 import "../css/Main.css";
 import { AuthContext } from "../App";
 import { getApi } from "../api";
+import Modal from 'react-modal';
 
 const getBadgeColor = (badge) => {
     switch (badge) {
@@ -23,7 +24,32 @@ const getBadgeColor = (badge) => {
     }
 };
 
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+
 const BadgeComp = () => {
+    var subtitle='';
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
     const authContext = useContext(AuthContext);
     const [userData, setUserData] = useState({ nickname: "", badge_color: "" });
     useEffect(() => {
@@ -48,7 +74,27 @@ const BadgeComp = () => {
     }, []);
     return (
         <div className="Main-BadgeComp">
-            <FaMedal color={userData.badge_color} size="4em" className="icon" />
+            <FaMedal onClick={openModal} color={userData.badge_color} size="4em" className="icon" />
+                <Modal
+                    isOpen={modalIsOpen}
+                    onAfterOpen={afterOpenModal}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    ariaHideApp={false}
+                    contentLabel="Example Modal"
+                >
+                    <h2 ref={_subtitle => (subtitle = _subtitle)}>뱃지 부여 기준</h2>
+                    <div>뱃지는 누적 총 공부시간으로 계산됩니다. <br></br>
+                        블랙(0시간 이상) <br></br>
+                        브론즈(5시간 이상) <br></br>
+                        실버(30시간 이상) <br></br>
+                        골드(100시간 이상) <br></br>
+                        플래티넘(200시간 이상) <br></br>
+                        다이아(1000시간 이상)<br></br>
+                    </div>
+                    <button onClick={closeModal}>close</button>
+                    
+                </Modal>
             <p>{userData.nickname}</p>
             <Link to="/study" className="button">
                 바로 공부하러 가기
