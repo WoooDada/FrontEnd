d0f956bff552916a642795fe9d4b11d9864b8f44
Jin Lee <leejin21c@gmail.com>
2021-08-03 21:12:14 +0900

    의미 없는 커밋

--- src/components/RightStudyComp.js
+++ src/components/RightStudyComp.js
@@ -20,12 +20,12 @@ let classifier;
 // 초 단위
 const MODEL_APPLY_TIME = 0.5;
 const POST_RESULT_TIME = 60;
-const GET_STUDYMATES = 60;
+const GET_STUDYMATES = 5;
 
 //////////////////////////////////////////////////////////////////////////////
 
 const RightStudyComp = ({ match }) => {
-    console.log('rightcomp match!', match); // match: room_id
+    console.log("rightcomp match!", match); // match: room_id
 
     const authContext = useContext(AuthContext);
     const btnContext = useContext(BtnContext);
@@ -42,47 +42,60 @@ const RightStudyComp = ({ match }) => {
 
     const [roomName, setRoomName] = useState("");
     const [roomTag, setRoomTag] = useState("");
-    const [roomManner, setRoomManner]
-        = useState("홀로로로로로롤 들어오세요 안녕안녕 다들 안녕 공부하자구요 규칙: 1. 아침 10시 기상 2.숙제해오기 3번 어쩌구 지키기 4. 이거 다 안지키면 삼진아웃입니다잉 아셨죠? 아웃이라고요 아웃!!! ");
+    const [roomManner, setRoomManner] = useState(
+        "홀로로로로로롤 들어오세요 안녕안녕 다들 안녕 공부하자구요 규칙: 1. 아침 10시 기상 2.숙제해오기 3번 어쩌구 지키기 4. 이거 다 안지키면 삼진아웃입니다잉 아셨죠? 아웃이라고요 아웃!!! "
+    );
     const [inppl, setInppl] = useState(0);
     const [maxppl, setMaxppl] = useState(0);
 
     // const [studymates, setStudymates] = useState([]);
 
-
     const initialStudymates = [
         {
-            nickname: '1인',
-            concent_rate: '45%',
-            concent_time: '1시간 32분',
-            play_time: '1시간 47분',
+            nickname: "1인",
+            concent_rate: "45%",
+            concent_time: "1시간 32분",
+            play_time: "1시간 47분",
         },
         {
-            nickname: '2인',
-            concent_rate: '90%',
-            concent_time: '2시간 42분',
-            play_time: '13분',
+            nickname: "2인",
+            concent_rate: "90%",
+            concent_time: "2시간 42분",
+            play_time: "13분",
         },
         {
-            nickname: '3인',
-            concent_rate: '45%',
-            concent_time: '1시간 32분',
-            play_time: '1시간 47분',
+            nickname: "3인",
+            concent_rate: "45%",
+            concent_time: "1시간 32분",
+            play_time: "1시간 47분",
         },
         {
-            nickname: '4인',
-            concent_rate: '90%',
-            concent_time: '2시간 42분',
-            play_time: '13분',
+            nickname: "4인",
+            concent_rate: "90%",
+            concent_time: "2시간 42분",
+            play_time: "13분",
         },
         {
-            nickname: '5인',
-            concent_rate: '90%',
-            concent_time: '2시간 42분',
-            play_time: '13분',
+            nickname: "5인",
+            concent_rate: "90%",
+            concent_time: "2시간 42분",
+            play_time: "13분",
         },
     ];
     const [studymates, setStudymates] = useState(initialStudymates);
+
+    // useEffect(() => {
+    //     // * 모델 불러오기 및 카메라 연결
+    //     classifier = ml5.imageClassifier("./model/model.json", () => {
+    //         navigator.mediaDevices
+    //             .getUserMedia({ video: true, audio: false })
+    //             .then((stream) => {
+    //                 videoRef.current.srcObject = stream;
+    //                 videoRef.current.play();
+    //                 setLoaded(true);
+    //             });
+    //     });
+    // }, []);
 
     useEffect(() => {
         // * 모델 불러오기 및 카메라 연결
@@ -95,9 +108,6 @@ const RightStudyComp = ({ match }) => {
                     setLoaded(true);
                 });
         });
-    }, []);
-
-    useEffect(() => {
         const getRoomInfo = async () => {
             const { status, data } = await getApi(
                 {
@@ -117,6 +127,34 @@ const RightStudyComp = ({ match }) => {
                 alert("네트워크 불안정");
             }
         };
+        const getStudymates = async () => {
+            const { status, data } = await getApi(
+                {
+                    room_id: match,
+                },
+                "/study/study_mate/",
+                authContext.state.token
+            );
+            // const { status, data } = { // Dummy
+            //     status: 200,
+            // };
+            console.log("status", status);
+            await console.log("get study mates");
+            await console.log(data);
+            if (status === 200) {
+                const new_studymates = data.studymates.map((sm, i) => ({
+                    nickname: sm.nickname,
+                    concent_rate: sm.concent_rate,
+                    concent_time: sm.concent_time,
+                    play_time: sm.play_time,
+                }));
+                console.log(new_studymates);
+                await setStudymates(new_studymates);
+            } else {
+                await alert("네트워크 오류");
+            }
+        };
+        getStudymates();
         getRoomInfo();
     }, []);
 
@@ -144,7 +182,7 @@ const RightStudyComp = ({ match }) => {
                         uid: authContext.state.uid,
                         type:
                             resultlistRef.current["C"] >
-                                resultlistRef.current["P"]
+                            resultlistRef.current["P"]
                                 ? "C"
                                 : "P",
                         time: new Date().toString().split(" ")[4].substr(0, 5),
@@ -209,21 +247,22 @@ const RightStudyComp = ({ match }) => {
     const [mannerMore, setMannerMore] = useState(false);
     const clickManner = () => {
         setMannerMore(!mannerMore);
-    }
+    };
 
     // 1분마다 studymates들 정보 받아오기
     useInterval(() => {
         const getStudymates = async () => {
-            const { status, data } = await postApi(
+            const { status, data } = await getApi(
                 {
                     room_id: match,
                 },
-                "/study/study_mates/",
+                "/study/study_mate/",
                 authContext.state.token
             );
-            // const { status, data } = { // Dummy 
+            // const { status, data } = { // Dummy
             //     status: 200,
             // };
+            await console.log(data);
             if (status === 200) {
                 await setStudymates(
                     data.studymates.map((sm, i) => ({
@@ -242,93 +281,97 @@ const RightStudyComp = ({ match }) => {
 
     const [matesIndex, setMatesIndex] = useState(0);
     const StudyMatesBox = ({ data }) => {
-        var crNum = data.concent_rate.slice(0,2);
-        var borderStyle = ''
+        console.log("data.concent_rate", data.concent_rate);
+        var crNum = data.concent_rate.slice(0, data.concent_rate - 1);
+        var borderStyle = "";
         if (crNum >= 50) {
-            borderStyle = { borderColor: '#92B355'}
+            borderStyle = { borderColor: "#92B355" };
         } else {
-            borderStyle = { borderColor: '#E9B2BC'}
+            borderStyle = { borderColor: "#E9B2BC" };
         }
 
         return (
             <div className="Studymates-box" style={borderStyle}>
                 <div className="Studymates-box-name">{data.nickname}</div>
                 <div className="Studymates-box-cr">{data.concent_rate} </div>
-                <div className="Studymates-box-ct">공부시간: {data.concent_time} </div>
-                <div className="Studymates-box-pt">딴짓시간: {data.play_time}</div>
+                <div className="Studymates-box-ct">
+                    공부시간: {data.concent_time}{" "}
+                </div>
+                <div className="Studymates-box-pt">
+                    딴짓시간: {data.play_time}
+                </div>
             </div>
-        )
-    }
+        );
+    };
 
     const StudyMatesBox3 = ({ datas }) => {
+        console.log("StudyMatesBox3", datas);
         return (
             <div className="Studymates-box3">
                 <StudyMatesBox data={datas[matesIndex]} />
                 <StudyMatesBox data={datas[matesIndex + 1]} />
                 <StudyMatesBox data={datas[matesIndex + 2]} />
             </div>
-        )
-    }
+        );
+    };
 
     const clickMatesLeftBtn = (e) => {
         e.preventDefault();
-        var leftBtn = document.getElementById('Studymates-leftbtn');
-        var rightBtn = document.getElementById('Studymates-rightbtn');
+        var leftBtn = document.getElementById("Studymates-leftbtn");
+        var rightBtn = document.getElementById("Studymates-rightbtn");
         // Studymates Index Change
         if (matesIndex !== 0) {
             setMatesIndex(matesIndex - 1);
         }
         // Button Color Change
         if (matesIndex <= 1) {
-            leftBtn.style.color = '#E1E5EA';
-            rightBtn.style.color = '#030303';
+            leftBtn.style.color = "#E1E5EA";
+            rightBtn.style.color = "#030303";
         } else {
-            leftBtn.style.color = '#030303';
-            rightBtn.style.color = '#030303';
+            leftBtn.style.color = "#030303";
+            rightBtn.style.color = "#030303";
         }
-
-    }
+    };
 
     const clickMatesRightBtn = (e) => {
         e.preventDefault();
-        var leftBtn = document.getElementById('Studymates-leftbtn');
-        var rightBtn = document.getElementById('Studymates-rightbtn');
+        var leftBtn = document.getElementById("Studymates-leftbtn");
+        var rightBtn = document.getElementById("Studymates-rightbtn");
         // Studymates Index Change
         if (matesIndex !== studymates.length - 3) {
             setMatesIndex(matesIndex + 1);
         }
         // Button Color Change
         if (matesIndex >= studymates.length - 4) {
-            leftBtn.style.color = '#030303';
-            rightBtn.style.color = '#E1E5EA';
+            leftBtn.style.color = "#030303";
+            rightBtn.style.color = "#E1E5EA";
         } else {
-            leftBtn.style.color = '#030303';
-            rightBtn.style.color = '#030303';
+            leftBtn.style.color = "#030303";
+            rightBtn.style.color = "#030303";
         }
-    }
+    };
 
     return (
         <div className="RightComp">
             <div className="RightComp-inner">
                 <div className="RightComp-roominfo-group1">
                     <div className="RightComp-roominfo-group2">
-                        <div className="RightComp-roomname">방이름{roomName}</div>
-                        <div className="RightComp-ppl">{inppl}/{maxppl}</div>
+                        <div className="RightComp-roomname">
+                            방이름{roomName}
+                        </div>
+                        <div className="RightComp-ppl">
+                            {inppl}/{maxppl}
+                        </div>
                     </div>
                     <div className="RightComp-roomtag">#수능{roomTag}</div>
                 </div>
 
                 <p className="RightComp-manner" onClick={(e) => clickManner()}>
-                    {
-                        roomManner.length > 40 ?
-                            (
-                                mannerMore ?
-                                    roomManner :
-                                    roomManner.substr(0, 37) + '...'
-                            )
-                            :
-                            roomManner
-                    }
+                    {roomManner.length > 40
+                        ? mannerMore
+                            ? roomManner
+                            : roomManner.substr(0, 37) + "..."
+                        : roomManner}
                 </p>
             </div>
             <div className="RightComp-inner">
@@ -363,18 +406,24 @@ const RightStudyComp = ({ match }) => {
                 <div className="RightComp-studymates">
                     <div className="RightComp-studymates-leftbtn">
                         <RiArrowLeftSLine
-                            id='Studymates-leftbtn'
-                            size='2rem'
+                            id="Studymates-leftbtn"
+                            size="2rem"
                             onClick={(e) => clickMatesLeftBtn(e)}
-                            style={{ color: '#E1E5EA' }} />
+                            style={{ color: "#E1E5EA" }}
+                        />
                     </div>
-                    <StudyMatesBox3 datas={studymates} />
+                    {studymates ? (
+                        <StudyMatesBox3 datas={studymates} />
+                    ) : (
+                        <div>친구 없엉 ㅠㅠ</div>
+                    )}
                     <div className="RightComp-studymates-rightbtn">
                         <RiArrowRightSLine
-                            id='Studymates-rightbtn'
-                            size='2rem'
+                            id="Studymates-rightbtn"
+                            size="2rem"
                             onClick={(e) => clickMatesRightBtn(e)}
-                            style={{ color: '#030303' }} />
+                            style={{ color: "#030303" }}
+                        />
                     </div>
                 </div>
             </div>
